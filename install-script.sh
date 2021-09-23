8d18b19f7aabde8b3f8fbc108c83e4e40aef8fd7
Ubuntu <ubuntu@ip-172-31-85-247.ec2.internal>
2021-09-23 07:39:30 +0000

    Pushing this script up to run on other services.

---
+++ install-script.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "Update apt"
+sudo apt update > /dev/null 2>&1 #Removing any extraneous output that I do not believe is neccesary This is seens everal times 
+isjava11installed=$?
+if [ $isjava11installed == 0 ]
+then
+  echo "java exists not updating java incase this causes an issue with Jenkins"
+elif [ $isjava11installed == 1 ]
+then
+  echo "Installing java"
+  sudo apt install openjdk-11-jdk -y > /dev/null 2>&1
+fi
+dpkg --list | grep -i openjdk-11 > /dev/null 2>&1
+echo "Adding keys to apt and new repo."
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add - #Idempotent will not keep adding  same key
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list' #idempotent will rewrite repo but does not matter
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9B7D32F2D50582E6 > /dev/null 2>&1 #idempotent will not keep adding same key
+sudo apt update > /dev/null 2>&1
+test -e /etc/default/jenkins
+doesjenkinsconfigexist=$?
+if [ $doesjenkinsconfigexist == 1 ]
+then
+  echo "Installing Jenkins"
+  sudo apt install jenkins -y > /dev/null 2>&1
+elif [ $doesjenkinsconfigexist == 0 ]
+then
+  echo "Jenkins is already installed not reinstalling jenkins"
+fi
+grep "HTTP_PORT=8000" /etc/default/jenkins > /dev/null 2>&1 #this is the port we want it to be set to so we are checking to see if its true
+didgrepsucceed=$?
+if [ $didgrepsucceed == 1 ]
+then
+  sudo sed -E -i s/HTTP\_PORT\=[0-9]+/HTTP\_PORT\=8000/ /etc/default/jenkins # This paired with the if command will replace any port number that isnt 8000
+  echo "Updating listening port to 8000 and restarting Jenkins"
+  sudo systemctl restart jenkins #restarting jenkins here to make sure the config is imported into the app
+elif [ $didgrepsucceed == 0 ] #If the port is already do not restart to avoid any interruptions
+then
+  echo "Jenkins listening port is already set to 8000 not modifying config" 
+fi
